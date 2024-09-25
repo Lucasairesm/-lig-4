4de0615533d327b1782798719590f3a3321712ac
hallerjunior@gmail.com <hallerjunior@hotmail.com>
2024-09-25 14:02:16 -0300

    Versão 2.0.0: - Integração do game com a switch - Inclusão da documentação no menu

---
+++ src/app/components/documentation-viewer/documentation-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentation-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './documentation-viewer.component.html',
+  styleUrl: './documentation-viewer.component.scss'
+})
+export class DocumentationViewerComponent {
+
+}
